9198d2ac5d01e99e0e4756d388c26da1db306226
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-05-02 19:03:09 +0000

    agregado css

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import './index.css'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<App />)
